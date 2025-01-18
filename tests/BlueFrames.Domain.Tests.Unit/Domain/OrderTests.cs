f376dccb0cbcf3cf97bbd89673072e645bd9730d
mdx <masouddehghani@hotmail.com>
2025-01-18 15:25:08 +0000

    Refactored order tests

--- tests/BlueFrames.Domain.Tests.Unit/Domain/OrderTests.cs
+++ tests/BlueFrames.Domain.Tests.Unit/Domain/OrderTests.cs
@@ -5,40 +5,34 @@ namespace BlueFrames.Domain.Tests.Unit.Domain;
 public class OrderTests
 {
     private readonly IDateTimeService _dateTimeService = Substitute.For<IDateTimeService>();
+    private readonly Guid _productId = Guid.NewGuid();
+    private readonly Guid _customerId = Guid.NewGuid();
+    private readonly DateTime _createdDate = new DateTime(2025, 01, 18, 14, 45, 0);
+
+    public OrderTests()
+    {
+        _dateTimeService.UtcNow.Returns(_createdDate);
+    }
     
     [Fact]
     public void CreateOrder_ShouldSucceed_WithValidData()
     {
-        // Arrange
-        var productId = Guid.NewGuid();
-        var customerId = Guid.NewGuid();
-        var createdDate = new DateTime(2025, 01, 18, 14, 45, 0);
-        
-        _dateTimeService.UtcNow.Returns(createdDate);
-        
         // Act
-        var order = new Order(productId, customerId, createdDate, _dateTimeService.UtcNow);
+        var order = new Order(_productId, _customerId, _createdDate, _dateTimeService.UtcNow);
 
         // Assert
         order.Should().NotBeNull();
-        order.ProductId.Should().Be(productId);
-        order.CustomerId.Should().Be(customerId);
-        order.CreatedDate.Should().Be(createdDate);
+        order.ProductId.Should().Be(_productId);
+        order.CustomerId.Should().Be(_customerId);
+        order.CreatedDate.Should().Be(_createdDate);
         order.UpdatedDate.Should().Be(DateTime.MinValue);
     }
 
     [Fact]
     public void CreateOrder_ShouldSuccess_WithPendingStatus()
     {
-        // Arrange
-        var productId = Guid.NewGuid();
-        var customerId = Guid.NewGuid();
-        var createdDate = new DateTime(2025, 01, 18, 14, 45, 0);
-        
-        _dateTimeService.UtcNow.Returns(createdDate);
-
         // Act
-        var order = new Order(productId, customerId, createdDate, _dateTimeService.UtcNow);
+        var order = new Order(_productId, _customerId, _createdDate, _dateTimeService.UtcNow);
         
         // Assert
         order.Status.Should().Be(Status.Pending);
@@ -48,14 +42,10 @@ public class OrderTests
     public void CreateOrder_ShouldFail_WhenProductIdIsInvalid()
     {
         // Arrange
-        var productId = Guid.Empty;
-        var customerId = Guid.NewGuid();
-        var createdDate = new DateTime(2025, 01, 18, 14, 45, 0);
-       
-        _dateTimeService.UtcNow.Returns(createdDate);
-
+        var invalidProductId = Guid.Empty;
+        
         // Act
-        Action createOrder = () => _ =new Order(productId, customerId, createdDate, _dateTimeService.UtcNow);
+        Action createOrder = () => _ =new Order(invalidProductId, _customerId, _createdDate, _dateTimeService.UtcNow);
         
         // Assert
         createOrder.Should().Throw<ValidationException>();
@@ -65,14 +55,10 @@ public class OrderTests
     public void CreateOrder_ShouldFail_WhenCustomerIdIsInvalid()
     {
         // Arrange
-        var productId = Guid.NewGuid();
-        var customerId = Guid.Empty;
-        var createdDate = new DateTime(2025, 01, 18, 14, 45, 0);
-
-        _dateTimeService.UtcNow.Returns(createdDate);
-
+        var invalidCustomerId = Guid.Empty;
+        
         // Act
-        Action createOrder = () => _ =new Order(productId, customerId, createdDate, _dateTimeService.UtcNow);
+        Action createOrder = () => _ =new Order(_productId, invalidCustomerId, _createdDate, _dateTimeService.UtcNow);
         
         // Assert
         createOrder.Should().Throw<ValidationException>();
@@ -82,14 +68,10 @@ public class OrderTests
     public void CreateOrder_ShouldFail_WhenCreatedDateIsInvalid()
     {
         // Arrange
-        var productId = Guid.NewGuid();
-        var customerId = Guid.NewGuid();
-        var createdDate = DateTime.MinValue;
-
-        _dateTimeService.UtcNow.Returns(new DateTime(2025, 01, 18, 14, 45, 0));
-
+        var invalidCreatedDate = new DateTime(2020, 01, 18, 14, 45, 0);
+        
         // Act
-        Action createOrder = () => _ =new Order(productId, customerId, createdDate, _dateTimeService.UtcNow);
+        Action createOrder = () => _ =new Order(_productId, _customerId, invalidCreatedDate, _dateTimeService.UtcNow);
         
         // Assert
         createOrder.Should().Throw<ValidationException>();
