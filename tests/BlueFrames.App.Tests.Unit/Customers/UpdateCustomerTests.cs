ac277741c3558c5da6189d590218e56d7130b1cb
mdx <masouddehghani@hotmail.com>
2025-01-18 22:42:41 +0000

    Refactored update customer tests

--- tests/BlueFrames.App.Tests.Unit/Customers/UpdateCustomerTests.cs
+++ tests/BlueFrames.App.Tests.Unit/Customers/UpdateCustomerTests.cs
@@ -6,74 +6,65 @@ namespace BlueFrames.App.Tests.Unit.Customers;
 
 public class UpdateCustomerTests
 {
+    private readonly CancellationToken _cancellationToken = CancellationToken.None;
+    private readonly ICustomerRepository _repository = Substitute.For<ICustomerRepository>();
+    private readonly IUnitOfWork _unitOfWork = Substitute.For<IUnitOfWork>();
+
+    private readonly Customer _customer = Customer.Create(
+        FirstName.From("John"),
+        LastName.From("Doe"),
+        PhoneNumber.From("07563385651"),
+        Email.From("john@doe.com"));
+    
+    public UpdateCustomerTests()
+    {
+        _repository.GetByIdAsync(_customer.Id.Value, _cancellationToken).Returns(_customer);
+    }
+    
     [Fact]
     public async Task UpdateCustomer_ShouldSuccess_WhenGivenValidData()
     {
         // Arrange
-        var cancellationToken = CancellationToken.None;
-        
-        var customer = Customer.Create(
-            FirstName.From("John"),
-            LastName.From("Doe"),
-            PhoneNumber.From("07563385651"),
-            Email.From("john@doe.com"));
-        
-        var repository = Substitute.For<ICustomerRepository>();
-        repository.GetByIdAsync(customer.Id.Value, cancellationToken).Returns(customer);
-        
-        var unitOfWork = Substitute.For<IUnitOfWork>();
-        unitOfWork.SaveChangesAsync(cancellationToken).Returns(1);
+        _unitOfWork.SaveChangesAsync(_cancellationToken).Returns(1);
         
         var updateCustomer = new UpdateCustomerCommand(
-            customer.Id.Value,
+            _customer.Id.Value,
             "Jane",
             "Doee",
             "07512345671",
             "jan@doee.com");
-        var updateHandler = new UpdateCustomerCommandHandler(repository, unitOfWork);
+        var updateHandler = new UpdateCustomerCommandHandler(_repository, _unitOfWork);
         
         // Act
-        var updateResult = await updateHandler.Handle(updateCustomer, cancellationToken);
+        var updateResult = await updateHandler.Handle(updateCustomer, _cancellationToken);
         
         // Assert
         updateResult.IsSuccess.Should().BeTrue();
         updateResult.Value.Should().NotBeEmpty();
-        updateResult.Value.Should().Be(customer.Id.Value);
+        updateResult.Value.Should().Be(_customer.Id.Value);
         
-        customer.FirstName.Value.Should().Be(updateCustomer.FirstName);
-        customer.LastName.Value.Should().Be(updateCustomer.LastName);
-        customer.Phone.Value.Should().Be(updateCustomer.Phone);
-        customer.Email.Value.Should().Be(updateCustomer.Email);
+        _customer.FirstName.Value.Should().Be(updateCustomer.FirstName);
+        _customer.LastName.Value.Should().Be(updateCustomer.LastName);
+        _customer.Phone.Value.Should().Be(updateCustomer.Phone);
+        _customer.Email.Value.Should().Be(updateCustomer.Email);
     }
     
     [Fact]
     public async Task UpdateCustomer_ShouldFail_WhenGivenInvalidData()
     {
         // Arrange
-        var cancellationToken = CancellationToken.None;
-        
-        var customer = Customer.Create(
-            FirstName.From("John"),
-            LastName.From("Doe"),
-            PhoneNumber.From("07563385651"),
-            Email.From("john@doe.com"));
-        
-        var repository = Substitute.For<ICustomerRepository>();
-        repository.GetByIdAsync(customer.Id.Value, cancellationToken).Returns(customer);
-        
-        var unitOfWork = Substitute.For<IUnitOfWork>();
-        unitOfWork.SaveChangesAsync(cancellationToken).Returns(1);
+        _unitOfWork.SaveChangesAsync(_cancellationToken).Returns(1);
         
         var updateCustomer = new UpdateCustomerCommand(
-            customer.Id.Value,
+            _customer.Id.Value,
             "Jane",
             "Doee",
             "07512345671",
             "");
-        var updateHandler = new UpdateCustomerCommandHandler(repository, unitOfWork);
+        var updateHandler = new UpdateCustomerCommandHandler(_repository, _unitOfWork);
         
         // Act
-        var updateResult = await updateHandler.Handle(updateCustomer, cancellationToken);
+        var updateResult = await updateHandler.Handle(updateCustomer, _cancellationToken);
         
         // Assert
         updateResult.IsSuccess.Should().BeFalse();
@@ -84,19 +75,7 @@ public class UpdateCustomerTests
     public async Task UpdateCustomer_ShouldFail_WhenCustomerNotFound()
     {
         // Arrange
-        var cancellationToken = CancellationToken.None;
-        
-        var customer = Customer.Create(
-            FirstName.From("John"),
-            LastName.From("Doe"),
-            PhoneNumber.From("07563385651"),
-            Email.From("john@doe.com"));
-        
-        var repository = Substitute.For<ICustomerRepository>();
-        repository.GetByIdAsync(customer.Id.Value, cancellationToken).Returns(customer);
-        
-        var unitOfWork = Substitute.For<IUnitOfWork>();
-        unitOfWork.SaveChangesAsync(cancellationToken).Returns(1);
+        _unitOfWork.SaveChangesAsync(_cancellationToken).Returns(1);
         
         var updateCustomer = new UpdateCustomerCommand(
             Guid.NewGuid(),
@@ -104,10 +83,10 @@ public class UpdateCustomerTests
             "Doee",
             "07512345671",
             "jan@doee.com");
-        var updateHandler = new UpdateCustomerCommandHandler(repository, unitOfWork);
+        var updateHandler = new UpdateCustomerCommandHandler(_repository, _unitOfWork);
         
         // Act
-        var updateResult = await updateHandler.Handle(updateCustomer, cancellationToken);
+        var updateResult = await updateHandler.Handle(updateCustomer, _cancellationToken);
         
         // Assert
         updateResult.IsSuccess.Should().BeFalse();
