912dcdd263a105cd1a22d02ab8a56dc1e87a1661
mdx <masouddehghani@hotmail.com>
2025-01-21 20:55:11 +0000

    Create product should return unauthorised when user is not authenticated

--- tests/BlueFrames.Api.Tests.Integration/ProductController/CreateProductControllerTests.cs
+++ tests/BlueFrames.Api.Tests.Integration/ProductController/CreateProductControllerTests.cs
@@ -4,20 +4,21 @@ namespace BlueFrames.Api.Tests.Integration.ProductController;
 
 public class CreateProductControllerTests : IClassFixture<BlueFramesApiFactory>
 {
-    private readonly HttpClient _adminHttpClient;
-
     private const int ProductSKUCharacterCount = 5;
     private readonly Faker<ProductRequest> _productFaker = new Faker<ProductRequest> ("en_GB")
         .RuleFor(dto => dto.Name, f => f.Commerce.ProductName())
         .RuleFor(dto => dto.Description, f => f.Commerce.ProductDescription())
         .RuleFor(dto => dto.SKU, f => f.Random.AlphaNumeric(ProductSKUCharacterCount).ToUpper());
 
+    private readonly HttpClient _adminHttpClient;
     private readonly HttpClient _userHttpClient;
+    private readonly HttpClient _httpClient;
 
     public CreateProductControllerTests(BlueFramesApiFactory factory)
     {
         _adminHttpClient = factory.CreateHttpClientWithAdminCredentials();
         _userHttpClient = factory.CreateHttpClientWithUserCredentials();
+        _httpClient = factory.CreateClient();
     }
     
     [Fact]
@@ -66,5 +67,18 @@ public class CreateProductControllerTests : IClassFixture<BlueFramesApiFactory>
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+    
+    [Fact]
+    public async Task Create_ShouldReturnUnauthorized_WhenUserIsNotAuthenticated()
+    {
+        // Arrange
+        var product = _productFaker.Generate();
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("/api/v1/Product", product);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
 }
