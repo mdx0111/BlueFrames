5da7bb137ee565e8e24e2217a711f1fb8ee238f1
mdx <masouddehghani@hotmail.com>
2025-01-21 17:51:37 +0000

    Get all customers should return forbidden when user is not admin

--- tests/BlueFrames.Api.Tests.Integration/CustomerController/GetCustomerControllerTests.cs
+++ tests/BlueFrames.Api.Tests.Integration/CustomerController/GetCustomerControllerTests.cs
@@ -6,17 +6,19 @@ namespace BlueFrames.Api.Tests.Integration.CustomerController;
 [TestCaseOrderer("BlueFrames.Api.Tests.Integration.Helpers.PriorityOrderer", "BlueFrames.Api.Tests.Integration")]
 public class GetCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
 {
-    private readonly HttpClient _httpClient;
-
     private readonly Faker<CustomerRequest> _customerFaker = new Faker<CustomerRequest>("en_GB")
         .RuleFor(dto => dto.FirstName, faker => faker.Person.FirstName)
         .RuleFor(dto => dto.LastName, faker => faker.Person.LastName)
         .RuleFor(dto => dto.Phone, faker => faker.Phone.PhoneNumberFormat(1).Replace(" ", string.Empty))
         .RuleFor(dto => dto.Email, faker => faker.Person.Email);
-    
+
+    private readonly HttpClient _adminHttpClient;
+    private readonly HttpClient _userHttpClient;
+
     public GetCustomerControllerTests(BlueFramesApiFactory factory)
     {
-        _httpClient = factory.CreateHttpClientWithAdminCredentials();
+        _adminHttpClient = factory.CreateHttpClientWithAdminCredentials();
+        _userHttpClient = factory.CreateHttpClientWithUserCredentials();
     }
 
     [Fact, TestPriority(4)]
@@ -24,12 +26,12 @@ public class GetCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
     {
         // Arrange
         var customer = _customerFaker.Generate();
-        var createResponse = await _httpClient.PostAsJsonAsync("/api/v1/Customer", customer);
+        var createResponse = await _adminHttpClient.PostAsJsonAsync("/api/v1/Customer", customer);
         var customerResponse = await createResponse.Content.ReadFromJsonAsync<Envelope>();
         var customerId = customerResponse.Result;
 
         // Act
-        var response = await _httpClient.GetAsync($"/api/v1/Customer/{customerId}");
+        var response = await _adminHttpClient.GetAsync($"/api/v1/Customer/{customerId}");
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
@@ -48,7 +50,7 @@ public class GetCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
     public async Task Get_ShouldReturnNotFound_WhenCustomerDoesNotExist()
     {
         // Act
-        var response = await _httpClient.GetAsync($"/api/v1/Customer/{Guid.NewGuid()}");
+        var response = await _adminHttpClient.GetAsync($"/api/v1/Customer/{Guid.NewGuid()}");
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
@@ -60,7 +62,7 @@ public class GetCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
     public async Task GetAll_ShouldNotReturnCustomers_WhenCustomerDoesNotExist()
     {
         // Act
-        var response = await _httpClient.GetAsync("/api/v1/Customer?offset=0&limit=10");
+        var response = await _adminHttpClient.GetAsync("/api/v1/Customer?offset=0&limit=10");
     
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
@@ -73,12 +75,12 @@ public class GetCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
     {
         // Arrange
         var customer = _customerFaker.Generate();
-        var createResponse = await _httpClient.PostAsJsonAsync("/api/v1/Customer", customer);
+        var createResponse = await _adminHttpClient.PostAsJsonAsync("/api/v1/Customer", customer);
         var customerResponse = await createResponse.Content.ReadFromJsonAsync<Envelope>();
         var customerId = customerResponse.Result;
 
         // Act
-        var response = await _httpClient.GetAsync("/api/v1/Customer?offset=0&limit=10");
+        var response = await _adminHttpClient.GetAsync("/api/v1/Customer?offset=0&limit=10");
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
@@ -92,5 +94,15 @@ public class GetCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
             Phone = customer.Phone,
             Email = customer.Email
         });
+    }
+    
+    [Fact]
+    public async Task GetAll_ShouldReturnForbidden_WhenUserIsNotAdmin()
+    {
+        // Act
+        var response = await _userHttpClient.GetAsync("/api/v1/Customer?offset=0&limit=10");
+    
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
     }
 }
