2e94271698e2e0abfd36768c29f86ad42ca2463d
mdx <masouddehghani@hotmail.com>
2025-01-21 17:47:34 +0000

    Delete customer should return forbidden if user is not admin

--- tests/BlueFrames.Api.Tests.Integration/CustomerController/DeleteCustomerControllerTests.cs
+++ tests/BlueFrames.Api.Tests.Integration/CustomerController/DeleteCustomerControllerTests.cs
@@ -4,17 +4,19 @@ namespace BlueFrames.Api.Tests.Integration.CustomerController;
 
 public class DeleteCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
 {
-    private readonly HttpClient _httpClient;
-
     private readonly Faker<CustomerRequest> _customerFaker = new Faker<CustomerRequest>("en_GB")
         .RuleFor(dto => dto.FirstName, faker => faker.Person.FirstName)
         .RuleFor(dto => dto.LastName, faker => faker.Person.LastName)
         .RuleFor(dto => dto.Phone, faker => faker.Phone.PhoneNumberFormat(1).Replace(" ", string.Empty))
         .RuleFor(dto => dto.Email, faker => faker.Person.Email);
-    
+
+    private readonly HttpClient _userHttpClient;
+    private readonly HttpClient _adminHttpClient;
+
     public DeleteCustomerControllerTests(BlueFramesApiFactory factory)
     {
-        _httpClient = factory.CreateHttpClientWithAdminCredentials();
+        _adminHttpClient = factory.CreateHttpClientWithAdminCredentials();
+        _userHttpClient = factory.CreateHttpClientWithUserCredentials();
     }
     
     [Fact]
@@ -22,12 +24,12 @@ public class DeleteCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
     {
         // Arrange
         var customer = _customerFaker.Generate();
-        var createResponse = await _httpClient.PostAsJsonAsync("/api/v1/Customer", customer);
+        var createResponse = await _adminHttpClient.PostAsJsonAsync("/api/v1/Customer", customer);
         var customerResponse = await createResponse.Content.ReadFromJsonAsync<Envelope>();
         var customerId = customerResponse.Result;
         
         // Act
-        var response = await _httpClient.DeleteAsync($"/api/v1/Customer/{customerId}");
+        var response = await _adminHttpClient.DeleteAsync($"/api/v1/Customer/{customerId}");
         
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
@@ -37,11 +39,27 @@ public class DeleteCustomerControllerTests : IClassFixture<BlueFramesApiFactory>
     public async Task Delete_ShouldReturnNotFound_WhenCustomerDoesNotExist()
     {
         // Act
-        var response = await _httpClient.DeleteAsync($"/api/v1/Customer/{Guid.NewGuid()}");
+        var response = await _adminHttpClient.DeleteAsync($"/api/v1/Customer/{Guid.NewGuid()}");
         
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
         var validationError = await response.Content.ReadFromJsonAsync<Envelope>();
         validationError.Errors["error"][0].Should().Contain("Customer not found");
+    }
+    
+    [Fact]
+    public async Task Delete_ShouldReturnForbidden_WhenUserIsNotAdmin()
+    {
+        // Arrange
+        var customer = _customerFaker.Generate();
+        var createResponse = await _adminHttpClient.PostAsJsonAsync("/api/v1/Customer", customer);
+        var customerResponse = await createResponse.Content.ReadFromJsonAsync<Envelope>();
+        var customerId = customerResponse.Result;
+        
+        // Act
+        var response = await _userHttpClient.DeleteAsync($"/api/v1/Customer/{customerId}");
+        
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
     }
 }
